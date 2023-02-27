63d4998a996102a61165e0370f95173604f3da39
Ilkhan <ilhanchanel20002@gmail.com>
2023-02-28 01:43:07 +0300

    Fix docker-compose 2

--- server/core/settings/development.py
+++ server/core/settings/development.py
@@ -10,7 +10,7 @@ LOGGING_CONFIG = None
 logging.config.fileConfig('logging.conf')
 
 POSTGRES_DB = environ.get('POSTGRES_DB', 'django')
-POSTGRES_HOST = environ.get('POSTGRES_HOST', 'database')
+POSTGRES_HOST = environ.get('POSTGRES_HOST', 'mvp_database')
 POSTGRES_USER = environ.get('POSTGRES_USER', 'django')
 POSTGRES_PASSWORD = environ.get('POSTGRES_PASSWORD', 'django')
 POSTGRES_PORT = environ.get('POSTGRES_PORT', '5432')
